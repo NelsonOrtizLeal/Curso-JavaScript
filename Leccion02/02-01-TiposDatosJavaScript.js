230055043198f64011eca8bef2123992c9272196
Nelson Ysain Ortiz Leal <nelson.ortiz@corporativoenciso.com>
2022-09-20 11:10:01 -0500

    Declarando variables de los datos mas usados en JavaScript

---
+++ Leccion02/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,21 @@
+/*
+Esto es un ejemplo de comentarios 
+de varias lineas
+*/
+
+//Tipos de dato string
+let nombre = "Nelson";
+console.log(nombre);
+
+//Tipo de dato numerico
+let numero = 100;
+console.log(numero)
+
+//Tipo de dato Object
+var persona = {
+    nombre: "Nelson",
+    apellido: "Ortiz",
+    edad: 24,
+    sexo: 'M'
+}
+console.log(persona)
