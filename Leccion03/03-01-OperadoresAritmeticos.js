5e482eaca82f860b5d3f6cea7299fac2e610ce30
Nelson Ysain Ortiz Leal <nelson.ortiz@corporativoenciso.com>
2022-09-20 16:57:35 -0500

    Uso de operadores aritmetios en JavaScript

---
+++ Leccion03/03-01-OperadoresAritmeticos.js
@@ -0,0 +1,19 @@
+let a = 3,
+    b = 2;
+let z = a + b;
+console.log("El resultado de la suma es " + z);
+
+z = a - b;
+console.log("El resultado de la resta es " + z);
+
+z = a * b;
+console.log("El resultado de la multiplicacion es " + z);
+
+z = a / b;
+console.log("El resultado de la division es " + z);
+
+z = a % b;
+console.log("El resultado de la operacion modulo " + z);
+
+z = a ** b;
+console.log("El resultado de operador exponente es " + z);
