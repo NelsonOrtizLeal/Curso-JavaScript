02403deee8566f0c168d1cdab618ff24222c2733
Nelson Ysain Ortiz Leal <nelson.ortiz@corporativoenciso.com>
2022-09-20 11:22:37 -0500

    Usando la funcion typeof para identificar el tipo de dato de una variable

--- Leccion02/02-01-TiposDatosJavaScript.js
+++ Leccion02/02-01-TiposDatosJavaScript.js
@@ -9,7 +9,7 @@ console.log(nombre);
 
 //Tipo de dato numerico
 let numero = 100;
-console.log(numero)
+console.log(numero);
 
 //Tipo de dato Object
 var persona = {
@@ -18,4 +18,17 @@ var persona = {
     edad: 24,
     sexo: 'M'
 }
-console.log(persona)
+console.log(persona);
+
+/*
+Existe una funcion que nos permite saber que tipo de dato es una variable
+la funcion se llama: typeof(tu_variable)
+la cual devuelve el tipo de dato
+*/
+
+let variable = 24;
+console.log(typeof(variable));
+
+//Las variables hacer dinamicas pueden cambiar su contenido
+variable = "Hola mundo";
+console.log(typeof(variable));
