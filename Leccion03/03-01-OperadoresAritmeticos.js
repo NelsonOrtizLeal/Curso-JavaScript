206718c2841c376b1e0c96148561c5df20c6568c
Nelson Ysain Ortiz Leal <nelson.ortiz@corporativoenciso.com>
2022-09-20 17:14:00 -0500

    Uso de Incremento y decremento en JavaScript

--- Leccion03/03-01-OperadoresAritmeticos.js
+++ Leccion03/03-01-OperadoresAritmeticos.js
@@ -17,3 +17,25 @@ console.log("El resultado de la operacion modulo " + z);
 
 z = a ** b;
 console.log("El resultado de operador exponente es " + z);
+
+//Incremento
+//Pre-incremento (el operador ++ antes de la variable)
+z = ++a; //suma 1 unidad y despues Asigna
+console.log(a);
+console.log(z);
+
+//Post-incremento (el operador ++ despues de la variable)
+z = b++; //Asigna y despues suma 1 unidad
+console.log(b);
+console.log(z);
+
+//Decremento
+//Pre-decremento
+z = --a; //Resta 1 unidad y despues Asigna
+console.log(a);
+console.log(z);
+
+//Post-decremento
+z = b--; //Asigna y despues resta 1 unidad
+console.log(b);
+console.log(z);
